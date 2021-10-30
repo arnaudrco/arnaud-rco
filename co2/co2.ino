b92e9b5ce6c9e3459c8dc254ffb5bf474d8ffdfc
reichart <90700891+arnaudrco@users.noreply.github.com>
2021-10-30 14:08:21 +0200

    Add files via upload

---
+++ co2/co2.ino
@@ -0,0 +1,151 @@
+#include <Adafruit_GFX.h>
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
+#include <TouchScreen.h>
+#define MINPRESSURE 200
+#define MAXPRESSURE 1000
+
+#include <Arduino.h>
+#include "MHZ19.h"                                        
+#include <SoftwareSerial.h>                                // Remove if using HardwareSerial
+
+#define RX_PIN 0                                         // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN 1                                        // Tx pin which the MHZ19 Rx pin is attached to
+#define BAUDRATE 9600                                      // Device to MH-Z19 Serial baudrate (should not be changed)
+
+MHZ19 myMHZ19;                                             // Constructor for library
+SoftwareSerial mySerial(RX_PIN, TX_PIN);                   // (Uno example) create device to MH-Z19 serial
+
+unsigned long getDataTimer = 0;
+
+
+// ALL Touch panels and wiring is DIFFERENT
+// copy-paste results from TouchScreen_Calibr_native.ino
+//const int XP = 6, XM = A2, YP = A1, YM = 7; //ID=0x9341
+//const int TS_LEFT = 907, TS_RT = 136, TS_TOP = 942, TS_BOT = 139;
+
+const int XP=8,XM=A2,YP=A3,YM=9; //240x320 ID=0x9341
+const int TS_LEFT=163,TS_RT=909,TS_TOP=170,TS_BOT=927;
+
+int CO2; 
+int8_t Temp;
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+Adafruit_GFX_Button on_btn, off_btn;
+
+int pixel_x, pixel_y;     //Touch_getXY() updates global vars
+bool Touch_getXY(void)
+{
+    TSPoint p = ts.getPoint();
+    pinMode(YP, OUTPUT);      //restore shared pins
+    pinMode(XM, OUTPUT);
+    digitalWrite(YP, HIGH);   //because TFT control pins
+    digitalWrite(XM, HIGH);
+    bool pressed = (p.z > MINPRESSURE && p.z < MAXPRESSURE);
+    if (pressed) {
+        pixel_x = map(p.x, TS_LEFT, TS_RT, 0, tft.width()); //.kbv makes sense to me
+        pixel_y = map(p.y, TS_TOP, TS_BOT, 0, tft.height());
+    }
+    return pressed;
+}
+
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+void setup(void)
+{
+ //  Serial.begin(9600);                                     // Device to serial monitor feedback
+
+    mySerial.begin(BAUDRATE);                               // (Uno example) device to MH-Z19 serial start   
+    myMHZ19.begin(mySerial);                                // *Serial(Stream) refence must be passed to library begin(). 
+
+    myMHZ19.autoCalibration();                              // Turn auto calibration ON (OFF autoCalibration(false))
+
+    uint16_t ID = tft.readID();
+//    Serial.print("TFT ID = 0x");
+//    Serial.println(ID, HEX);
+//    Serial.println("Calibrate for your Touch Panel");
+    if (ID == 0xD3D3) ID = 0x9486; // write-only shield
+    tft.begin(ID);
+    tft.setRotation(0);            //PORTRAIT
+    tft.fillScreen(BLACK);
+    on_btn.initButton(&tft,  60, 200, 100, 40, WHITE, CYAN, BLACK, "ON", 2);
+    off_btn.initButton(&tft, 180, 200, 100, 40, WHITE, CYAN, BLACK, "OFF", 2);
+    on_btn.drawButton(false);
+    off_btn.drawButton(false);
+    tft.fillRect(40, 80, 160, 80, RED);
+
+}
+
+/* two buttons are quite simple
+ */
+
+void co2(){   // affichage du taux de CO2
+              tft.fillRect(0, 0, 240, 80, BLACK);
+     tft.setTextSize(4);
+        tft.setTextColor(YELLOW);
+          tft.setCursor(0, 0);
+    tft.print("CO2:");
+    tft.println(CO2);
+    
+   tft.print("T:"); 
+    tft.print(Temp);
+    tft.println(" C");
+   
+}
+
+ 
+void loop(void)
+{
+
+ if (millis() - getDataTimer >= 2000)
+    {
+
+
+        /* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even 
+        if below background CO2 levels or above range (useful to validate sensor). You can use the 
+        usual documented command with getCO2(false) */
+
+        CO2 = myMHZ19.getCO2();                             // Request CO2 (as ppm)
+        co2();
+ //       Serial.print("CO2 (ppm): ");                      
+ //       Serial.println(CO2);                                
+
+
+        Temp = myMHZ19.getTemperature();                     // Request Temperature (as Celsius)
+ //       Serial.print("Temperature (C): ");                  
+  //      Serial.println(Temp);                               
+
+        getDataTimer = millis();
+    }
+    bool down = Touch_getXY();
+    on_btn.press(down && on_btn.contains(pixel_x, pixel_y));
+    off_btn.press(down && off_btn.contains(pixel_x, pixel_y));
+    if (on_btn.justReleased())
+        on_btn.drawButton();
+    if (off_btn.justReleased()){
+              off_btn.drawButton();
+             
+    }
+        
+    if (on_btn.justPressed()) {
+        on_btn.drawButton(true);
+        tft.fillRect(40, 80, 160, 80, GREEN);
+    }
+    if (off_btn.justPressed()) {
+        off_btn.drawButton(true);
+        tft.fillRect(40, 80, 160, 80, RED);
+
+        
+    }
+
+    
+
+}
